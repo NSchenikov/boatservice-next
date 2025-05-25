fb97e5a3352aa4173a7052bfd4791a487c50b759
Nikita-Schenikov <nikita-shenikov@mail.ru>
2025-05-25 21:55:24 +0300

    logo added in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Image from 'next/image';
 
 const Nav = styled.nav`
   position: fixed;
@@ -6,14 +7,36 @@ const Nav = styled.nav`
   width: 100%;
   background: white;
   display: flex;
-  justify-content: center;
-  padding: 1rem;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.75rem 2rem;
   z-index: 1000;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 `;
 
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    height: 36px;
+    width: auto;
+  }
+
+  span {
+    margin-left: 0.5rem;
+    font-weight: bold;
+    color: var(--primary);
+    font-size: 1.1rem;
+  }
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+`;
+
 const NavLink = styled.a`
-  margin: 0 1rem;
+  margin-left: 1rem;
   font-weight: bold;
   color: var(--primary);
 
@@ -25,10 +48,17 @@ const NavLink = styled.a`
 export default function Header() {
   return (
     <Nav>
-      <NavLink href="#about">О нас</NavLink>
-      <NavLink href="#location">Как нас найти</NavLink>
-      <NavLink href="#gallery">Галерея</NavLink>
-      <NavLink href="#order">Заказать</NavLink>
+      <Logo>
+        <Image src="/images/logo/logo.png" alt="Логотип" width={36} height={36} />
+        <span>Прокат лодок, сапов, катамаранов Переславль</span>
+      </Logo>
+
+      <NavLinks>
+        <NavLink href="#about">О нас</NavLink>
+        <NavLink href="#location">Как нас найти</NavLink>
+        <NavLink href="#gallery">Галерея</NavLink>
+        <NavLink href="#order">Заказать</NavLink>
+      </NavLinks>
     </Nav>
   );
 }
