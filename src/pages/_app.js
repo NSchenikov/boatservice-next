eae91975a77e3d958bba2029eed8f474fb7f2ee9
Nikita-Schenikov <nikita-shenikov@mail.ru>
2025-08-07 23:14:05 +0300

    add template Schema.org for LocalBusiness

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import GlobalStyle from '../styles/GlobalStyle';
 
 export default function MyApp({ Component, pageProps }) {
@@ -13,6 +14,35 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'LocalBusiness',
+              name: 'Прокат лодок, сапов и катамаранов в Переславле',
+              image: 'https://supboatpz.ru/images/logo/logo.png',
+              url: 'https://supboatpz.ru',
+              telephone: '+7 (915) 973-88-04',
+              address: {
+                '@type': 'PostalAddress',
+                streetAddress: 'ул. Левая Набережная, рядом с домом 107',
+                addressLocality: 'Переславль-Залесский',
+                addressCountry: 'RU',
+              },
+              geo: {
+                '@type': 'GeoCoordinates',
+                latitude: 56.739,
+                longitude: 38.859,
+              },
+              openingHours: 'Mo-Su 09:00-21:00',
+              sameAs: [],
+            }),
+          }}
+        />
+      </Head>
+
       <GlobalStyle />
       <Component {...pageProps} />
     </>
